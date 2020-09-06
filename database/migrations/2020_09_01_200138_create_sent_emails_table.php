822d53b793dab5d2ad124a7b6499649cd237ee0b
andrew_marhai <buk2018irinam@gmail.com>
2020-09-06 21:30:06 +0300

    Обновил Readme и сделал правку в названии класса миграции

--- database/migrations/2020_09_01_200138_create_sent_emails_table.php
+++ database/migrations/2020_09_01_200138_create_sent_emails_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMailsLogTable extends Migration
+class CreateSentEmailsTable extends Migration
 {
     /**
      * Создание таблицы для логирования информации об отправленных письмах
